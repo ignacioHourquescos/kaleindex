6660317cf490635669f0f7dc343fe3972bc66e74
Alejodg9 <digiovine.a@gmail.com>
2021-08-12 20:39:20 -0300

    habemus header

--- section/Header/Header.js
+++ section/Header/Header.js
@@ -1,15 +1,29 @@
 import ss from './Header.module.scss'
 
-const Header =() =>{
+const Header = () => {
 
-return(
-   <>
-      
-      <div style={{backgroundColor: "green", height:"100vh", color:"white", textAlign:"center"}}>
-         HEader
-      </div>
-   </>
-)
+   return (
+      <>
+         <div className={ss.container}>
+            <div className={ss.herokale}>
+            </div>
+               <div className={ss.herotitle}>
+                  <div className={ss.letter}>X</div>
+                  <div className={ss.letter}>E</div>
+                  <div className={ss.letter}>D</div>
+                  <div className={ss.letter}>N</div>
+                  <div className={ss.letter}>I</div>
+                  <span className={ss.green}>
+                     <div className={ss.letter}>E</div>
+                     <div className={ss.letter}>L</div>
+                     <div className={ss.letter}>A</div>
+                     <div className={ss.letter}>K</div>
+                  </span>
+               
+            </div>
+         </div>
+      </>
+   )
 
 }
 
