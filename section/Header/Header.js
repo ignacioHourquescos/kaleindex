8e5a4f466811ef4ed7d409669d1b567b0e69b9a8
Alejodg9 <digiovine.a@gmail.com>
2021-08-12 21:17:37 -0300

    header desktop first try

--- section/Header/Header.js
+++ section/Header/Header.js
@@ -4,9 +4,10 @@ const Header = () => {
 
    return (
       <>
-         <div className={ss.container}>
-            <div className={ss.herokale}>
-            </div>
+         <div className={ss.desktopview}>
+            <div className={ss.container}>
+               <div className={ss.herokale}>
+               </div>
                <div className={ss.herotitle}>
                   <div className={ss.letter}>X</div>
                   <div className={ss.letter}>E</div>
@@ -19,7 +20,11 @@ const Header = () => {
                      <div className={ss.letter}>A</div>
                      <div className={ss.letter}>K</div>
                   </span>
-               
+
+               </div>
+            </div>
+            <div className={ss.left}>
+               <p className={ss.herodesc}>Combinamos <span className={ss.green}>innovación</span>, <span className={ss.green}>lógica</span> y <span className={ss.green}>diseño</span> para crear productos digitales de alto valor agregado. Siempre abierto a nuevos desafios!</p>
             </div>
          </div>
       </>
