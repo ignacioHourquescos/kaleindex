931da8165f341af0e780ef343247c7bb53470ea7
Jimancio <54536722+ignacioHourquescos@users.noreply.github.com>
2021-08-14 15:06:11 -0300

    cards funiconado

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,19 @@
 
 import Header from '../section/Header/Header'
 import Portfolio from '../section/Portfolio/Portfolio'
-
+import Card from '../section/Portfolio2/Portfolio2'
 
 const Home = () => {
   return (
     <>
       <Header/>
-      <Portfolio/>   
+      <Card/>   
+      <Card/>   
+      <Card/>   
+      <Card/>   
+      <Card/>   
+      <Card/>   
+      {/* <Portfolio/>    */}
 
     </>
   )
