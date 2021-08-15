65ef687d6fe2bc3db75219d377167130ddcbb657
Alejodg9 <digiovine.a@gmail.com>
2021-08-14 23:10:03 -0300

    scroll snap bitch

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,17 @@
 
 import Header from '../section/Header/Header'
-
 import Card from '../section/Portfolio/Portfolio'
 
 const Home = () => {
   return (
     <>
-      <Header/>
-      <Card/>   
+
+      <section style={{ scrollSnapAlign: "start" }}>
+        <Header />
+      </section>
+      <section style={{ scrollSnapAlign: "start" }}>
+        <Card />
+      </section>
 
       {/* <Portfolio/>    */}
 
