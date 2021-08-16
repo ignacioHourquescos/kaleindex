8182d7d9790d404c8e6058b4bddd5bc900587c8d
Alejodg9 <digiovine.a@gmail.com>
2021-08-16 15:48:47 -0300

    hero section mobile ok y downarrow ok

--- section/Header/Header.js
+++ section/Header/Header.js
@@ -24,24 +24,38 @@ const Header = () => {
                {
                   showLogo
                      ?
+                     <>
+                        <div className={ss.herotitle}>
+                           <div className={ss.letter} style={{ transform: "rotate(-90deg)", animation: "fadeInDown", animationDuration: "1.4s" }}>X</div>
+                           <div className={ss.letter} style={{ transform: "rotate(-90deg)", animation: "fadeInDown", animationDuration: "1.3s" }}>E</div>
+                           <div className={ss.letter} style={{ transform: "rotate(-90deg)", animation: "fadeInDown", animationDuration: "1.2s" }}>D</div>
+                           <div className={ss.letter} style={{ transform: "rotate(-90deg)", animation: "fadeInDown", animationDuration: "1.1s" }}>N</div>
+                           <div className={ss.letter} style={{ transform: "rotate(-90deg)", animation: "fadeInDown", animationDuration: "1s", textAlign: "center" }}>I</div>
+                           <span className={ss.green}>
+                              <div className={ss.letter} style={{ transform: "rotate(-90deg)", animation: "fadeInDown", animationDuration: "0.9s" }}>E</div>
+                              <div className={ss.letter} style={{ transform: "rotate(-90deg)", animation: "fadeInDown", animationDuration: "0.8s" }}>L</div>
+                              <div className={ss.letter} style={{ transform: "rotate(-90deg)", animation: "fadeInDown", animationDuration: "0.7s" }}>A</div>
+                              <div className={ss.letter} style={{ transform: "rotate(-90deg)", animation: "fadeInDown", animationDuration: "0.6s" }}>K</div>
+                           </span>
+                           <div className={ss.mouse_scroll}>
+                              <div>
+                                 <span className={`${ss.m_scroll_arrows} ${ss.unu}`}></span>
+                                 <span className={`${ss.m_scroll_arrows} ${ss.doi}`}></span>
+                                 <span className={`${ss.m_scroll_arrows} ${ss.trei}`}></span>
+                              </div>
+                           </div>
+                        </div>
 
-                     <div className={ss.herotitle}>
-                        <div className={ss.letter} style={{ transform: "rotate(-90deg)", animation: "fadeInDown", animationDuration: "1.4s" }}>X</div>
-                        <div className={ss.letter} style={{ transform: "rotate(-90deg)", animation: "fadeInDown", animationDuration: "1.3s" }}>E</div>
-                        <div className={ss.letter} style={{ transform: "rotate(-90deg)", animation: "fadeInDown", animationDuration: "1.2s" }}>D</div>
-                        <div className={ss.letter} style={{ transform: "rotate(-90deg)", animation: "fadeInDown", animationDuration: "1.1s" }}>N</div>
-                        <div className={ss.letter} style={{ transform: "rotate(-90deg)", animation: "fadeInDown", animationDuration: "1s", textAlign: "center" }}>I</div>
-                        <span className={ss.green}>
-                           <div className={ss.letter} style={{ transform: "rotate(-90deg)", animation: "fadeInDown", animationDuration: "0.9s" }}>E</div>
-                           <div className={ss.letter} style={{ transform: "rotate(-90deg)", animation: "fadeInDown", animationDuration: "0.8s" }}>L</div>
-                           <div className={ss.letter} style={{ transform: "rotate(-90deg)", animation: "fadeInDown", animationDuration: "0.7s" }}>A</div>
-                           <div className={ss.letter} style={{ transform: "rotate(-90deg)", animation: "fadeInDown", animationDuration: "0.6s" }}>K</div>
-                        </span>
-                     </div>
+
+
+                     </>
+
                      :
                      ""
 
                }
+
+
             </div>
             <div className={ss.left}>
                <p className={ss.herodesc}>Combinamos <span className={ss.green}>innovación</span>, <span className={ss.green}>lógica </span>
