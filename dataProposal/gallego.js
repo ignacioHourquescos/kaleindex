66225602c9841563a0bcd4621035f6287fde9c1a
Alejodg9 <digiovine.a@gmail.com>
2021-08-17 00:00:50 -0300

    arreglos presupuesto trasnoche

--- dataProposal/gallego.js
+++ dataProposal/gallego.js
@@ -1,24 +1,24 @@
 const gallego = {
    number: "121",
-   nosotros:`Hola!.`,
+   nosotros: `Hola!.`,
 
 
-   ustedes:``,
+   ustedes: ``,
 
-   alcance:{
+   alcance: {
 
       resumen: `Pagina web a medida institucional a medida, contemplando
       las mejores practicas de diseño e usabilidad.
       teniendo como referencia CeroliniFerrari / Estudio Ofarrel. `,
 
-      detalle:[
+      detalle: [
          `Sección principal`,
          `Sección quienes somos`,
          `Sección Nuestros servicios`,
-         `Sección Publicaciones`,     
+         `Sección Publicaciones`,
          `Formulario de contacto`,
       ],
-      tecnico:[
+      tecnico: [
          `Diseño hecho a medida, utilizando HTML5, CSS, y Java Script`,
          `Diseño 100% responsivo (adaptable a todas las pantallas)`,
          `Sección de publicaciones autoadministrable`,
@@ -27,24 +27,24 @@ const gallego = {
    },
 
    etapas: {
-      investigacionDuration:`5 dias`,
+      investigacionDuration: `5 dias`,
       investigacion: `Etapa en la que buscamos referencias. Tanto de estudios colegas como de paginas
        de afuera. Se define a grandes rasgos cual es la identidad que se le va a dar a la web. Coposicion gráfica
        ,
        informacion,`,
-     
-      prototipadoDuration:`15 dias `,
+
+      prototipadoDuration: `15 dias `,
       prototipado: ` Se monta un prototipo con informacion de muestra de la pagina principal 
       para poder ver el look and feel de la web. Se discute cualquier ajuste que se pueda hacer `,
-      
-      betaDuration:`10 dias`,
+
+      betaDuration: `10 dias`,
       beta: `Se construye toda la web a un 80%. se definen ulitmos ajustes. Recibimos la informacio 
       que quedara en la pagina`,
-      
-      lanzamientoDuration:`5 dias`,
+
+      lanzamientoDuration: `5 dias`,
       lanzamiento: `Se lanza la web en el dominio deseado. `
    },
-   
+
    presupuesto: {
       investigacion: `5 hrs`,
 
@@ -52,9 +52,14 @@ const gallego = {
 
       desarollo: `20 hrs`,
 
-      valor:`45000`,
-   }
+      valor: `45000`,
 
+      noIncluye: [
+         `Dominio`,
+         `Hosting`,
+         `E-mail corporativo`,
+      ],
+   },
 }
 
 
