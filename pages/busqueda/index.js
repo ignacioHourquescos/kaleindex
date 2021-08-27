a539a91d137947e8a5fb264cd4b194976df64a7b
Jimancio <54536722+ignacioHourquescos@users.noreply.github.com>
2021-08-27 01:15:27 -0300

    busqueda

---
+++ pages/busqueda/index.js
@@ -0,0 +1,31 @@
+
+import Head from 'next/head'
+
+
+
+const Footer = () => {
+
+
+    return (
+        <>
+              <Head>
+         <title>KALE</title>
+         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+         <meta name="description" content="KALEINDEX: Agencia Creativa Digital" />
+         <meta property="og:title" content="UX DESIGNER" />
+         <meta property="og:description" content="Agencia creativa digital" />
+         <meta property="og:image" content="/kaleillustration.jpg" />
+      </Head>
+
+<iframe  src="https://docs.google.com/document/d/e/2PACX-1vSPeC-lNHg7jUyPVJ6UPfPgaTgSk1i-VpbFuNP6TLHaStThFYZ-OddGpdudaemunY3Y6LFJpi4EdIl1/pub?embedded=true" 	
+
+
+   style={{height:"100vh", width:"100%"}}></iframe>
+        </>
+
+
+    )
+
+}
+
+export default Footer;
