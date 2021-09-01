ab95a6a31d42bac38e896ff3567139ab5b87913b
Alejodg9 <digiovine.a@gmail.com>
2021-09-01 11:04:27 -0300

    agregado pre acttum

---
+++ pages/acttumPre/index.js
@@ -0,0 +1,31 @@
+
+import Head from 'next/head'
+
+
+
+const ActtumPre = () => {
+
+
+    return (
+        <>
+              <Head>
+         <title>KALE</title>
+         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+         <meta name="description" content="KALEINDEX: Agencia Creativa Digital" />
+         <meta property="og:title" content="UX DESIGNER" />
+         <meta property="og:description" content="Agencia creativa digital" />
+         <meta property="og:image" content="/kaleillustration.jpg" />
+      </Head>
+
+<iframe  src="https://docs.google.com/document/d/10qjAt_BpoxM7txeYB23fqk10uFZkZxhuSCvaBFQ6tic/edit?usp=sharing" 	
+
+
+   style={{height:"100vh", width:"100%"}}></iframe>
+        </>
+
+
+    )
+
+}
+
+export default ActtumPre;
