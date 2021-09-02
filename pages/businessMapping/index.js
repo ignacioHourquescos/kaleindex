d5b54d2714173166a4b7ce8f79c84e5d23198c31
Alejodg9 <digiovine.a@gmail.com>
2021-09-02 15:08:57 -0300

    agregado iframe bussines mapping

---
+++ pages/businessMapping/index.js
@@ -0,0 +1,29 @@
+
+import Head from 'next/head'
+
+
+
+const businessMapping = () => {
+
+
+    return (
+        <>
+            <Head>
+                <title>KALE</title>
+                <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+                <meta name="description" content="KALEINDEX: Agencia Creativa Digital" />
+                <meta property="og:title" content="UX DESIGNER" />
+                <meta property="og:description" content="Agencia creativa digital" />
+                <meta property="og:image" content="/kaleillustration.jpg" />
+            </Head>
+
+            <iframe src="https://docs.google.com/document/d/e/2PACX-1vTNMt05qd7BrOUgr8Zuku92ALsdBdrrS9PHnvVSeCvmxeqZcq3ZS7ztHklLrVNXS5G25OqaDfd5W38y/pub?embedded=true"
+                style={{ height: "100vh", width: "100%" }}></iframe>
+        </>
+
+
+    )
+
+}
+
+export default businessMapping;
