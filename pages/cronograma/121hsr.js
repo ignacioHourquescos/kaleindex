e254c5b16d7d6708aedce003248a2f763dfaab95
Jimancio <54536722+ignacioHourquescos@users.noreply.github.com>
2021-09-09 01:03:34 -0300

    cronograma hsr

--- pages/cronograma/121hsr.js
+++ pages/cronograma/121hsr.js
@@ -11,19 +11,23 @@ const ActtumPre = () => {
               <Head>
          <title>KALE</title>
          <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-         <meta name="description" content="KALEINDEX: Agencia Creativa Digital" />
-         <meta property="og:title" content="UX DESIGNER" />
+         <meta name="description" content="Cronograma proyecto" />
+         <meta property="og:title" content="CRONO HSR" />
          <meta property="og:description" content="Agencia creativa digital" />
          <meta property="og:image" content="/kaleillustration.jpg" />
       </Head>
-
-<iframe  src="https://docs.google.com/document/d/e/2PACX-1vR2AntUdL-JvVFjrkot8CfheU-UGM20havscdJixi6VgKKGWB0ueaAUL_2aaF5L5o_s0j67bo1nSmDj/pub?embedded=true"
+     
+      <iframe src="https://docs.google.com/spreadsheets/d/e/2PACX-1vTC5STAGWeRes6j2IRspTocjzm7YsOcNMEMUkHBl0nCk0SPsGQkSx0aMdoO7vzuDarZkJYGTaAwpqFh/pubhtml?gid=909818325&amp;single=true&amp;widget=true&amp;headers=false"
    style={{height:"100vh", width:"100%"}}></iframe>
-        </>
 
+   
+        </>
 
     )
 
 }
 
 export default ActtumPre;
+
+
+
