8469f3a3d5185ec6101007482cab958af346081a
Alejodg9 <digiovine.a@gmail.com>
2021-09-28 16:26:50 -0300

    presu dg arte segundo intento

---
+++ dataProposal/DGArte.js
@@ -0,0 +1,82 @@
+const acttum = {
+    number: "125DGa",
+    Bienvenidos:
+        `
+
+   Creemos que la participación del cliente es fundamental para generar procesos ágiles y 
+  un producto diferencial, que es lo que siempre buscamos.`
+
+
+
+    // gracias por considerarnos.
+    // sabemos que  necesitas tu pagina funcionando cuanto antes.
+    //  Nuestra participacion tanto como la de ustedes  es fundamental para que los procesos sean agiles y dinamicos
+    // y asi poder construir un producto de valor diferencial. A continuacion encontraran los detalles de su proyecto y el valor del mismo.
+    // Esperamos poder realizar este proyecto con ustedes!
+    //  `
+    ,
+
+    ustedes: ``,
+
+    alcance: {
+
+        resumen: `Diseño y programacion de portfolio de arte a medida, teniendo en cuenta las mejores prácticas de diseño y usabilidad.`,
+
+        detalle: [
+            `Sección home.`,
+            `Sección Series (x3).`,
+            `Sección quiénes soy.`,
+            `Contacto a wpp`,
+            
+            
+        ],
+        tecnico: [
+            `Desarrollo hecho a medida, utilizando HTML5, CSS, y JavaScript.`,
+            `Diseño 100% responsivo (adaptable a todas las pantallas).`,
+            `Optimizado para motores de búsqueda.`,
+        ],
+    },
+
+    etapas: {
+        /* investigacionDuration: `5 días`, */
+        investigacion: `Etapa en la que buscamos referencias de artistas del rubro tanto locales como extranjeros.
+       Se define en conjunto y a grandes rasgos cuál es la identidad que se le va a dar a la web. Composición gráfica
+       , paleta de colores, estilos. Pára que esta etapa se de por finalizada necesitamos contar con el siguiente material:`,
+        recursos: [
+            `Identidad`,
+            `Imágenes`,
+            `Textos`,
+        ],
+
+        /* prototipadoDuration: `15 días `, */
+        prototipado: ` Se monta un prototipo con información final de la página principal y las secciones
+      para poder ver el aspecto y sensaciones de la web. Se discute cualquier ajuste que se pueda hacer. `,
+
+        /* betaDuration: `10 días`, */
+        beta: `Se construye toda la web a un 100%. Se definen últimos ajustes.`,
+
+        /* lanzamientoDuration: `3 días`, */
+        lanzamiento: `Se lanza la web en el dominio deseado y se realiza un seguimiento estrecho del funcionamiento,
+        junto con cualquier ajuste final necesario. `
+    },
+
+    presupuesto: {
+        investigacion: ``,
+
+        diseño: ``,
+
+        desarollo: ``,
+
+        valor: `50.000`,
+
+        noIncluye: [
+            `Dominio`,
+            `Hosting`,
+            `E-mail corporativo`,
+        ],
+    },
+}
+
+
+
+export default acttum;
