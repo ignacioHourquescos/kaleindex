ba3a88306f94a514caa6abfe046bc2eb600bc605
Alejodg9 <digiovine.a@gmail.com>
2021-10-14 14:28:57 -0300

    generado presupuesto El Fuerte

---
+++ dataProposal/elFuerte.js
@@ -0,0 +1,87 @@
+const acttum = {
+    number: "126eF",
+    Bienvenidos:
+        `
+
+   Creemos que la participación del cliente es fundamental para generar procesos ágiles y 
+  un producto diferencial, que es lo que siempre buscamos.`
+
+
+
+    // gracias por considerarnos.
+    // sabemos que  necesitas tu pagina funcionando cuanto antes.
+    //  Nuestra participacion tanto como la de ustedes  es fundamental para que los procesos sean agiles y dinamicos
+    // y asi poder construir un producto de valor diferencial. A continuacion encontraran los detalles de su proyecto y el valor del mismo.
+    // Esperamos poder realizar este proyecto con ustedes!
+    //  `
+    ,
+
+    ustedes: ``,
+
+    alcance: {
+
+        resumen: `Diseño y programacion de un generador de pedidos conectado a la lista de precios, teniendo en cuenta las mejores prácticas de diseño y usabilidad.`,
+
+        detalle: [
+            `Sección Lista de precios interactiva con Carrito`,
+            `Sección Lista de precios version administrador`,
+
+
+
+        ],
+        tecnico: [
+            `Desarrollo hecho a medida, utilizando HTML5, CSS, y JavaScript.`,
+            `Diseño 100% responsivo (adaptable a todas las pantallas).`,
+
+        ],
+    },
+
+    etapas: {
+        /* investigacionDuration: `5 días`, */
+        investigacion: ``,
+        recursos: [
+            `Se definen las mejores prácticas del rubro entrevistado brevemente a 3 grandes clientes proporcionados por EL Fuerte.`,
+            `Se analizan buenos y malos ejemplosde pedidos que reciban via wpp, mail etc.`,
+            `Se buscan referencias de afuera u otros rubros /industrias`,
+        ],
+
+        /* prototipadoDuration: `15 días `, */
+        prototipado: `Se arma un wireframe (un boceto), donde se corrobora lo que va a ver el cliente y la información que recibe El Fuerte.
+         Este wireframe se convierte luego a un prototipo funcional para chequear la usabilidad`,
+        requisitosProto: [
+            `LADO CLIENTE: Planilla de precios con opción de agregar items a un carrito, según fraccionamiento predeterminado (1kg, 500g etc.)`,
+            `LADO EL FUERTE: Recibe los pedidos vía mail, vía sistema, via wpp. A definir canal y formato`,
+            
+        ],
+
+        /* betaDuration: `10 días`, */
+        beta: `Se construye una primera versión al 80% de acuerdo a lo relevado y se testea`,
+        testingBeta: [
+            `TESTING RUBRO: se testea con clientes proporcionados por EL Fuerte`,
+            `TESTING EXTERNO: KaleIndex testea con usuarios aleatorios`,
+            
+        ],
+        /* lanzamientoDuration: `3 días`, */
+        lanzamiento: `Se ajusta modelo de acuerdo a devolución de clientes y otros usuarios. Se testea en conjunto con El Fuerte el resultado del proceso. Se deja el producto funcionando`
+    },
+
+    presupuesto: {
+        investigacion: ``,
+
+        diseño: ``,
+
+        desarollo: ``,
+
+        valor: `40.000`,
+
+        noIncluye: [
+            `Dominio`,
+            `Hosting`,
+            `E-mail corporativo`,
+        ],
+    },
+}
+
+
+
+export default acttum;
