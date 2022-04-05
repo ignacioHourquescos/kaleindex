6dc78d102b6dd2f454785f9630717ba541cfc92e
Jimancio <54536722+ignacioHourquescos@users.noreply.github.com>
2022-04-05 10:41:14 -0300

    Update index.js

--- pages/busqueda/index.js
+++ pages/busqueda/index.js
@@ -12,12 +12,13 @@ const Footer = () => {
          <title>KALE</title>
          <meta name="viewport" content="initial-scale=1.0, width=device-width" />
          <meta name="description" content="KALEINDEX: Agencia Creativa Digital" />
-         <meta property="og:title" content="UX DESIGNER" />
+         <meta property="og:title" content="ESPECIALISTA SEO" />
          <meta property="og:description" content="Agencia creativa digital" />
          <meta property="og:image" content="/kaleillustration.jpg" />
       </Head>
 
-<iframe  src="https://docs.google.com/document/d/e/2PACX-1vSPeC-lNHg7jUyPVJ6UPfPgaTgSk1i-VpbFuNP6TLHaStThFYZ-OddGpdudaemunY3Y6LFJpi4EdIl1/pub?embedded=true" 	
+<iframe  src="https://docs.google.com/document/d/e/2PACX-1vTqjvs3IE3nZWp0xLoSiNwlrXEEG1SMJXK14F-iOOPWeemSkJZoMkbgi8Yn4i1F5XHSmTSfp0FCmQTr/pub?embedded=true" 
+
 
 
    style={{height:"100vh", width:"100%"}}></iframe>
