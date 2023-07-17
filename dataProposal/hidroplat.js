c0d98af8e1cce28a18cc7a8f886bdea176b3dd4c
Alejodg9 <digiovine.a@gmail.com>
2023-07-17 11:27:38 -0300

    proposal hidroplat

---
+++ dataProposal/hidroplat.js
@@ -0,0 +1,64 @@
+const acttum = {
+  number: "HPt135",
+  Bienvenidos: `
+
+   Creemos que la participación del cliente es fundamental para generar procesos ágiles y 
+  un producto diferencial, que es lo que siempre buscamos.`,
+
+  // gracias por considerarnos.
+  // sabemos que  necesitas tu pagina funcionando cuanto antes.
+  //  Nuestra participacion tanto como la de ustedes  es fundamental para que los procesos sean agiles y dinamicos
+  // y asi poder construir un producto de valor diferencial. A continuacion encontraran los detalles de su proyecto y el valor del mismo.
+  // Esperamos poder realizar este proyecto con ustedes!
+  //  `
+  ustedes: ``,
+
+  alcance: {
+    resumen: `Comunity Management de Redes + Creacion y diseño del contenido.`,
+
+    detalle: [
+      `2 Publicación en feed semanal.`,
+      `2 reel mensual.`,
+      `Stories`,
+      `Reporte mensual de estadísticas`,
+    ],
+    tecnico: [`Material audivisual diseñado en Figma y CapCut.`],
+  },
+
+  etapas: {
+    /* investigacionDuration: `5 días`, */
+    investigacion: `Etapa en la que buscamos referencias de artistas del rubro tanto locales como extranjeros.
+       Se define en conjunto y a grandes rasgos cuál es la identidad que se le va a dar a la web. Composición gráfica
+       , paleta de colores, estilos. Pára que esta etapa se de por finalizada necesitamos contar con el siguiente material:`,
+    recursos: [`Identidad`, `Imágenes`, `Textos`],
+
+    /* prototipadoDuration: `15 días `, */
+    prototipado: ` Se monta un prototipo con información final de la página principal y las secciones
+      para poder ver el aspecto y sensaciones de la web. Se discute cualquier ajuste que se pueda hacer. `,
+
+    /* betaDuration: `10 días`, */
+    beta: `Se construye toda la web a un 100%. Se definen últimos ajustes.`,
+
+    /* lanzamientoDuration: `3 días`, */
+    lanzamiento: `Se lanza la web en el dominio deseado y se realiza un seguimiento estrecho del funcionamiento,
+        junto con cualquier ajuste final necesario. `,
+  },
+
+  presupuesto: {
+    investigacion: ``,
+
+    diseño: ``,
+
+    desarollo: ``,
+
+    valor: `55.000`,
+
+    noIncluye: [
+      `Sesiones de foto o video para generar el material crudo. (En caso de no contar con este material podemos coordinar con estudios audiovisuales o freelancers de nuestra red)`,
+      `Publicidad paga`,
+      
+    ],
+  },
+};
+
+export default acttum;
